e0e170b953878f2240567abd12f4d698c6247690
Kamino <iden1107@icloud.com>
2022-11-05 20:43:00 +0900

    [modify]ダライトモードに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
     titleTemplate: '%s - vue-presentation',
     title: 'vue-presentation',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'ja'
     },
     meta: [
       { charset: 'utf-8' },
@@ -47,7 +47,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
